4a16c7e50dedd2eb22fe46a5516f39ef8251498f
luigics <luigics@al.insper.edu.br>
2024-05-17 13:57:01 -0300

    objetivos fix

--- js/script.js
+++ js/script.js
@@ -1,74 +1,67 @@
 // barra de progresso do index
 var currentLength;
-        function increaseLength(){
-            currentLength = document.getElementById("progress").value;
-            if (currentLength < 90) {
-                currentLength += 10;
-                document.getElementById("progress").value = currentLength;
-            }
-
-            else if (currentLength >= 90 && currentLength < 100) {
-                currentLength = 100;
-                document.getElementById("progress").value = currentLength;
-
-            }
-
-            else{
-                alert("You reached the maximum length of the progressbar!");
-            }
-        }
-
-        function decreaseLength(){
-            currentLength = document.getElementById("progress").value;
-            if (currentLength > 10) {
-                currentLength -= 10;
-                document.getElementById("progress").value = currentLength;
-            }
-
-            else if (currentLength > 0 && currentLength <= 10) {
-                currentLength = 0;
-                document.getElementById("progress").value = currentLength;
-            }
-
-            else{
-                alert("You reached the minimum length of the progressbar!");
-            }
-        }
-
-// script página de objetivos
-document.addEventListener('DOMContentLoaded', function() {
-
-
-    ul = document.querySelector('ul')
-    botao = document.querySelector('input[type="submit"]')
-    texto = document.querySelector('input[type="text"]')
-    lis = document.querySelectorAll('li')
-    i = 0
-    botao.addEventListener('click',function(event){
-        li = document.createElement('li')
-        li.innerHTML = texto.value
-        ul.appendChild(li)
-        ex = i
-
-        if (ex != i + 1){
-            ex = i += 1
-            localStorage.setItem(ex,li.innerHTML)
-
-        }
-        
-
-        event.preventDefault()
-    })
-
-    key = 1
-    while (key <= localStorage.length){
-        li = document.createElement('li')
-        texto = localStorage.getItem(key)
-        li.innerHTML = texto
-        ul.appendChild(li)
-        key += 1
+var ultima_posicao = 1;
+function increaseLength() {
+    currentLength = document.getElementById("progress").value;
+    if (currentLength < 90) {
+        currentLength += 10;
+        document.getElementById("progress").value = currentLength;
     }
 
+    else if (currentLength >= 90 && currentLength < 100) {
+        currentLength = 100;
+        document.getElementById("progress").value = currentLength;
+
+    }
+
+    else {
+        alert("You reached the maximum length of the progressbar!");
+    }
+}
+
+function decreaseLength() {
+    currentLength = document.getElementById("progress").value;
+    if (currentLength > 10) {
+        currentLength -= 10;
+        document.getElementById("progress").value = currentLength;
+    }
+
+    else if (currentLength > 0 && currentLength <= 10) {
+        currentLength = 0;
+        document.getElementById("progress").value = currentLength;
+    }
+
+    else {
+        alert("You reached the minimum length of the progressbar!");
+    }
+}
+
+// script página de objetivos
+document.addEventListener('DOMContentLoaded', function () {
+    let botao_adicionar = document.querySelector(".adicionar-elemento");
+    let botao_remover = document.querySelector(".remover-elemento");
+    
+    botao_adicionar.addEventListener("click", function () {
+        let input_text = document.querySelector("input[type=text]");
+        texto = input_text.value
+        chave = ultima_posicao + ""
+        localStorage.setItem(chave, texto);
+        ultima_posicao++;
+        
+        let novo_elemento = document.createElement("li");
+        novo_elemento.innerHTML = texto;
+        document.querySelector("ul").appendChild(novo_elemento);
+
+        input_text.value = "";
+    });
+
+    botao_remover.addEventListener("click", function () {
+        chave = ultima_posicao-1 + ""
+        localStorage.removeItem(chave);
+        ultima_posicao--;
+        
+        document.querySelector("ul").lastChild.remove();
+    });
 
 
-})
+});
